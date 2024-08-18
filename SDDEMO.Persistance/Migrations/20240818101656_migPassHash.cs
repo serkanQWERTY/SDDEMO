82e808bf69fa298ea359627ac077a4190d4a14e0
serkan.isde99@gmail.com <serkan.isde99@gmail.com>
2024-08-18 13:19:46 +0300

    passwordHash done

---
+++ SDDEMO.Persistance/Migrations/20240818101656_migPassHash.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SDDEMO.Persistance.Migrations
+{
+    /// <inheritdoc />
+    public partial class migPassHash : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "password",
+                table: "Users",
+                newName: "passwordHash");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "passwordHash",
+                table: "Users",
+                newName: "password");
+        }
+    }
+}
